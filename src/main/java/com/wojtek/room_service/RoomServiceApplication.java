4934632b4a3a21fe21204198a061e3b789d0ff6b
WojciechWeg <wojtek.weg@gmail.com>
2020-10-15 23:09:02 +0200

    add eureka support

--- src/main/java/com/wojtek/room_service/RoomServiceApplication.java
+++ src/main/java/com/wojtek/room_service/RoomServiceApplication.java
@@ -1,10 +1,21 @@
 package com.wojtek.room_service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Lazy;
+import com.netflix.discovery.EurekaClient;
 
 @SpringBootApplication
 public class RoomServiceApplication {
+
+	@Autowired
+	@Lazy
+	private EurekaClient eurekaClient;
+
+	@Value("${spring.application.name}")
+	private String appName;
 
 	public static void main(String[] args) {
 		SpringApplication.run(RoomServiceApplication.class, args);
